8dace0294407dcfb86bb83e719166c2e7f6b8fce
Robert Groot <github@rgroot.nl>
2021-07-01 23:30:55 +0200

    Config fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,55 +1,68 @@
-import { defineConfig, UserConfig, UserConfigExport } from 'vite';
+import { ConfigEnv, defineConfig, UserConfigExport } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import copy from 'rollup-plugin-copy';
 import pages from 'vite-plugin-pages';
 import eslintPlugin from 'vite-plugin-eslint';
+import compression from 'vite-plugin-compression';
 
 const public_routes = [
     /^\/login$/,
 ];
 
 // https://vitejs.dev/config/
-export default ({ mode }: UserConfig ): UserConfigExport => {
+export default ({mode, command}: ConfigEnv ): UserConfigExport => {
     // If you need env
     // process.env = {...process.env, ...loadEnv(mode, process.cwd())};
 
     const prod = mode === 'production';
+    const dev_server = command !== 'build';
+
+    const plugins = [
+        vue(),
+        pages({
+            pagesDir: 'src/pages',
+            extensions: ['vue', 'ts'],
+            importMode() {
+                return 'async';
+            },
+            extendRoute(route) {
+                if (public_routes.some(public_route => public_route.test(route.path))) {
+                    return route;
+                }
+
+                return {
+                    ...route,
+                    meta: { auth: true },
+                };
+            },
+        }),
+        eslintPlugin({
+            fix: ! prod,
+            include: ['./**/*.ts', './**/*.vue']
+        }),
+    ];
+
+    if (prod) {
+        plugins.push(compression({filter: (): boolean => true}));
+        plugins.push({
+            ...copy({
+                targets: [
+                    { src: 'resources/vue/dist/*', dest: 'public/vue' },
+                ],
+                overwrite: true,
+                hook: 'writeBundle',
+            }),
+            enforce: 'post',
+        });
+    }
 
     return defineConfig({
-        plugins: [
-            vue(),
-            pages({
-                pagesDir: 'src/pages',
-                extensions: ['vue', 'ts'],
-                importMode() {
-                    return 'async';
-                },
-                extendRoute(route) {
-                    if (public_routes.some(public_route => public_route.test(route.path))) {
-                        return route;
-                    }
-
-                    return {
-                        ...route,
-                        meta: { auth: true },
-                    };
-                },
-            }),
-            eslintPlugin({
-                fix: ! prod,
-                include: ['./**/*.ts', './**/*.vue']
-            }),
-            copy({
-                targets: ! prod ? [] : [
-                    { src: 'resources/vue/dist/*', dest: 'public/vue' },
-                ]
-            }),
-        ],
+        plugins: plugins,
         root: 'resources/vue',
         build: prod ? {} : {
             outDir: '../../public/vue',
         },
-        base: prod ? '/vue/' : '/',
+        base: dev_server ? '/' : '/vue/',
         server: {
             host: '0.0.0.0',
         },
