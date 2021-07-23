06db83eda392a0f1a8479ca22bed7d4ad474d8f6
RobertGroot <gitlab@rgroot.nl>
2021-07-23 11:12:49 +0200

    Composer updates

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Http\Response;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -11,6 +12,12 @@ abstract class TestCase extends BaseTestCase
     public function setUp(): void
     {
         parent::setUp();
+
+        abort_if(
+            method_exists($this, 'usingInMemoryDatabase') && !$this->usingInMemoryDatabase(),
+            Response::HTTP_UNPROCESSABLE_ENTITY,
+            'Config uses real database instead of in-memory sqlite.'
+        );
 
         $this->app->make(\Spatie\Permission\PermissionRegistrar::class)->registerPermissions();
     }
